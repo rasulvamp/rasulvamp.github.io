a4ac471a67acf8170a9b3e9a82de886506087780
rasulvamp <rasul.bairamov.98@mail.ru>
2026-08-17 09:29:21 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const SITE = {
   seaDistance: '150 м',
   beds: '4',
   parking: '3 машины',
-  locationName: 'СНТ «Золотой берег»',
+  locationName: 'СНТ «Дагестанские Огни»',
   coordinates: '42.148948, 48.227941',
   phone1: '+79772674550',
   phone1Display: '+7 (977) 267-45-50',
